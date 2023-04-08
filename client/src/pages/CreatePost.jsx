4a6b28845476137b3c902919ffb206749d843735
Raphael Agbara <ralphagbara@email.com>
2023-04-08 11:20:14 +0100

    added more components and logic

--- client/src/pages/CreatePost.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CreatePost = () => {
-  return (
-    <div>createPost</div>
-  )
-}
-
-export default CreatePost;
--- client/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React, {useState,useEffects}from 'react'
-
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
-
-export default Home
